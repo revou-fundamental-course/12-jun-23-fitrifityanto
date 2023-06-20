c22c9b016af62034e769900d8f687b72f3fcb442
fitrifityanto <fitrifityanto@gmail.com>
2023-06-20 19:27:33 +0700

    perbaiki validate: menambahkan validasi isNaN

--- js/script.js
+++ js/script.js
@@ -8,46 +8,41 @@ function onConvert() {
     let tempNameFirst = temperatureFirst.dataset.tempFirst
 
     let tempFirstValue = tempFirst.value
-    let tempSecondValue = tempSecond.value
 
-    let temperature = {
-        tempFirstValue: tempFirstValue,
-        tempSecondValue: tempSecondValue,
+    let error = validate()
+
+    if (error.tempFirstValue) {
+        document.getElementById('errorMsg').textContent = error.tempFirstValue
+        document.getElementById('errorMsg').style.visibility = 'visible'
     }
 
-    let error = validate(temperature)
-
-    if (tempNameFirst === 'celsius') {
-        if (temperature.tempFirstValue !== '') {
-            let tempSecond = (tempFirstValue * 9/5) + 32
-            document.getElementById('tempSecond').value = tempSecond
-            document.getElementById('errorMsg').textContent = ''
-            document.getElementById('kalkulasi').value = `(${tempFirstValue} \u00B0C x 9/5) + 32 = ${tempSecond} \u00B0F `
+    else if (!error.tempFirstValue) {
+        if (tempNameFirst === 'celsius') {
+                let tempSecond = (tempFirstValue * 9/5) + 32
+                document.getElementById('tempSecond').value = tempSecond
+                document.getElementById('kalkulasi').value = `(${tempFirstValue} \u00B0C x 9/5) + 32 = ${tempSecond} \u00B0F `
+            }
     
-        } else {
-            document.getElementById('errorMsg').textContent = error.tempFirstValue
-        }
-
-    } else if (tempNameFirst === 'fahrenheit')  {
-        if (temperature.tempFirstValue !== '') {
-            let tempSecond = (tempFirstValue - 32) * 5/9
-            document.getElementById('tempSecond').value = tempSecond
-            document.getElementById('errorMsg').textContent = ''
-            document.getElementById('kalkulasi').value = `(${tempFirstValue} \u00B0F - 32) x 5/9 = ${tempSecond} \u00B0C `
+        else if (tempNameFirst === 'fahrenheit')  {
+                let tempSecond = (tempFirstValue - 32) * 5/9
+                document.getElementById('tempSecond').value = tempSecond
     
-        } else {
-            document.getElementById('errorMsg').textContent = error.tempFirstValue
-            console.log('kosong')
+                document.getElementById('kalkulasi').value = `(${tempFirstValue} \u00B0F - 32) x 5/9 = ${tempSecond} \u00B0C `
         }
+        document.getElementById('errorMsg').textContent = ''
+        document.getElementById('errorMsg').style.visibility = 'hidden'
     }
 }
 
 function validate() {
     let error = {}
-    let tempFirstValue = tempFirst.value
+    const tempFirstValue = tempFirst.value
 
     if (!tempFirstValue) {
         error.tempFirstValue = 'Input tidak boleh kosong'
+    } 
+    else if (isNaN(tempFirstValue)) {
+        error.tempFirstValue = 'Input harus angka'
     }
 
     return error
@@ -60,6 +55,7 @@ function onReverse() {
     tempSecond.value = ''
     document.getElementById('kalkulasi').value = ''
     document.getElementById('errorMsg').textContent = ''
+    document.getElementById('errorMsg').style.visibility = 'hidden'
 
     if (tempNameFirst == 'celsius') {
         temperatureFirst.dataset.tempFirst = 'fahrenheit'
@@ -82,6 +78,7 @@ function onReset() {
     tempSecond.value = ''
     document.getElementById('kalkulasi').value = ''
     document.getElementById('errorMsg').textContent = ''
+    document.getElementById('errorMsg').style.visibility = 'hidden'
 
     temperatureFirst.dataset.tempFirst = 'celsius'
     temperatureFirst.textContent = 'Celsius (\u00B0C)'
@@ -93,7 +90,6 @@ function onReset() {
 
 // script untuk accordion explanation
 const accordion = document.getElementsByClassName('container-explain')
-console.log(accordion)
 
 for (i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener('click', function() {
